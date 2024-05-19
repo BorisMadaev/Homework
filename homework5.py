fc2a1e970715816ac1a5d1464161c469f8fc0168
BorisMadaev <madaevb@yandex.ru>
2024-05-20 05:23:13 +0800

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,17 @@
+immutable_var = (1, True, "Привет!")
+print(immutable_var)
+# immutable_var [0] = 3
+# Ошибка!
+# отдельный элемент кортежа менять нельзя, надо перезаменять весь кортеж целиком.
+# Пример,
+# immutable_var = (3, True, "Привет!")
+# print(immutable_var)
+# Исключение - если в кортеж входит список, внутренние элементы списка можно отдельно менять.
+# Пример:
+# immutable_var = ([1,1], True, "Привет!")
+# print(immutable_var)
+# immutable_var [0][0] = 3
+# print(immutable_var)
+mutable_list = [1, 15, "Апельсин"]
+mutable_list[1] = True
+print(mutable_list)
