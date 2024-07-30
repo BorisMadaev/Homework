63f5f6f4d50d1deaf93a1981ec4eb76ceae5979d
BorisMadaev <madaevb@yandex.ru>
2024-07-31 07:34:36 +0800

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,29 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(new_floor):
+                print(i + 1)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return "Название " + str(self.name) + ", кол-во этажей: " + str(self.number_of_floors)
+
+
+h1 = House('ЖК Эльбрус', 30)
+h2 = House("Первый", 18)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
